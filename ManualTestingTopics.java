2b14717046f54d6121c6219fb3c8fc23e4b9aeaf
Devina <devina@Devinas-MacBook-Air.local>
2022-01-04 21:11:24 +0000

    this is my first commit

---
+++ ManualTestingTopics.java
@@ -0,0 +1,17 @@
+package com.myfirstprogram;
+
+public class ManualTestingTopics {
+    public static void main (String[]arg) {
+        System.out.println("SoftwareTesting");
+System.out.print("Software");
+System.out.print(" Development");
+System.out.println(" LifeCycle");
+System.out.println("Testing Techniques");
+System.out.println("Definition Of Done");
+System.out.print("Test");
+System.out.print(" Driven Development");
+System.out.println(" Principals oF Software testing");
+System.out.print("Test");
+System.out.println(" Plan");
+    }
+}
